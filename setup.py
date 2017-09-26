db8fb8cf9e89b57188a737396564f480a84a7f3c
Kris Ciccarello <kciccarello@gmail.com>
2017-09-26 15:41:34 -0400

    Cloak version 0.1

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cloak',
+    version='0.1',
+    description='Feature toggling in Python based on feature_ramp.',
+    url='https://github.com/wisr/cloak',
+    author='Kris Ciccarello',
+    author_email='kciccarello@gmail.com',
+    license='MIT',
+    packages=[''],
+    zip_safe=False
+)
